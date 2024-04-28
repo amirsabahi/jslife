a119002375c7c301d605dae93a1ef70633d6c335
Amir Sabahi <amir.sabahi@gmail.com>
2024-04-28 21:06:18 +0330

    Update LinkedList.js

--- LinkedList.js
+++ LinkedList.js
@@ -2,7 +2,7 @@ class Node{
   constructor(value)
   {
     this.value = value
-    this.next = null
+    this.next=null
   }
 }
 class LinkedList 
@@ -11,9 +11,10 @@ class LinkedList
   {
     this.head = {
       value: value,
-      next: null
+      next:null
     } 
     this.tail = this.head
+    this.length = 0
   }
   
   append(value)
@@ -21,14 +22,50 @@ class LinkedList
     const newNode = new Node(value)
     this.tail.next = newNode
     this.tail = newNode
+    this.length++
   }
   prepand(value)
   {
     const newNode = new Node(value)
     newNode.next = this.head
     this.head = newNode
+    this.length++
   }
   
+  printList()
+  {
+    
+    let currentNode = this.head
+    while(currentNode != null)
+    {
+        console.log(currentNode.value)    
+        currentNode = currentNode.next
+    }
+  }
+  
+  insert(index, value)
+  {
+    if(index>= this.length)
+    {
+      this.append(value)    
+    }
+    let currentNode = this.head
+    let currentIndex = 0
+    while(currentNode != null)
+    {
+        if(currentIndex == index) 
+         {
+           let next = currentNode.next
+           let newNode = new Node(value)
+           currentNode.next = newNode
+           newNode.next = next
+           this.length++
+          break   
+         }
+        currentIndex++;
+        currentNode = currentNode.next
+    }
+  }
 }
 
 const myLinkedList = new LinkedList(10)
@@ -36,4 +73,6 @@ const myLinkedList = new LinkedList(10)
 myLinkedList.append(5)
 myLinkedList.append(16)
 myLinkedList.prepand(1)
-console.log("1", myLinkedList)
+myLinkedList.insert(2, 15)
+myLinkedList.insert(200, 200)
+myLinkedList.printList()
