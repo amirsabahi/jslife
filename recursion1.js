7df5b49e8ce04ff7e9fe3ac8b0d2b625d6c1ed8e
amirsabahi <ah.sabahi@outlook.com>
2024-05-12 13:09:36 +0330

    Add recusrsion example

---
+++ recursion1.js
@@ -0,0 +1,23 @@
+const array = [
+    1,
+    2,
+    3,
+    [4, 5, 6],
+    7,
+    [8, [9, 10, 11, [12, 13, 14]]],
+    [15, 16, 17, 18, 19, [20, 21, 22, [23, 24, 25, [26, 27, 29]], 30, 31], 32],
+    33
+  ];
+  
+  function traverseArray(array) {
+    for (let item of array) {
+      if (typeof item === "object") {
+        traverseArray(item);
+      } else {
+        console.log(item);
+      }
+    }
+  }
+  
+  traverseArray(array);
+  
